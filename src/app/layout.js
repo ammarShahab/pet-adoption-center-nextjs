523ffb8e96e66713185a7bce55a681f950634210
unknown <Administrator@ELLIASH-QA.generalpharma.com>
2025-08-21 12:42:07 +0600

    created login page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
-import Banner from "./components/Banner";
 import Footer from "./components/Footer";
 
 const geistSans = Geist({
@@ -26,7 +25,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar></Navbar>
-        <main className="h-screen place-content-center ">{children}</main>
+        <main className="min-h-screen place-content-center ">{children}</main>
         <Footer />
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,11 @@
-import Image from "next/image";
 import Banner from "./components/Banner";
 import ProductHighlight from "./components/ProductHighlight";
-import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <div>
       <Banner />
       <ProductHighlight />
-      {/* <Footer /> */}
     </div>
   );
 }
