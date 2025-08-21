d29e5dee95509f6a5cf3164d93167aca19472dbb
unknown <Administrator@ELLIASH-QA.generalpharma.com>
2025-08-21 09:55:00 +0600

    created navbar using daisy ui

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -3,8 +3,59 @@ import LoginBTN from "./LoginBTN";
 import Link from "next/link";
 
 const Navbar = () => {
+  const links = (
+    <>
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <li>
+        <Link href="/allpets">Pets</Link>
+      </li>
+      <li>
+        <Link href="/addpets">Add Pets</Link>
+      </li>
+    </>
+  );
   return (
-    <nav className="flex justify-around items-center max-w-7xl mx-auto py-6 bg-amber-300">
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {links}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{links}</ul>
+      </div>
+      <div className="navbar-end">
+        <Link href="/login">
+          <LoginBTN></LoginBTN>
+        </Link>
+      </div>
+    </div>
+    /*  <nav className="flex justify-around items-center max-w-7xl mx-auto py-6 bg-amber-300">
       <ul className="flex justify-center items-center gap-10">
         <Link href="/">
           <li>Home</li>
@@ -19,7 +70,7 @@ const Navbar = () => {
       <Link href="/login">
         <LoginBTN></LoginBTN>
       </Link>
-    </nav>
+    </nav> */
   );
 };
 
